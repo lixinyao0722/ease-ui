05d73e6725b9dd7ae8a8cad91eb9678544e6e3cf
xinyaoli <xinyao.li@shopee.com>
2019-04-23 10:02:20 +0800

    mark Component

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
 </template>
 
 <script lang="ts">
-  import { Vue } from 'vue-property-decorator';
+  import { Vue, Component } from 'vue-property-decorator';
 
+  @Component
   export default class App extends Vue {
 
   }
